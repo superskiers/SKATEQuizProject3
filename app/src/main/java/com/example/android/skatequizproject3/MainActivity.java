1173364fef9c632f90e6a16722e828a489de65a2
superskiers <superskiers718@gmail.com>
2018-03-02 09:24:47 +0100

    just go

--- app/src/main/java/com/example/android/skatequizproject3/MainActivity.java
+++ app/src/main/java/com/example/android/skatequizproject3/MainActivity.java
@@ -12,14 +12,24 @@ import android.widget.Toast;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+
     //Declare Variables
     private VideoView vv;
     private RadioButton rbKickFLIP;
-    private RadioButton ques3Radio1;
+    private RadioButton q3RB2;
     private RadioButton frontFootPush;
     private RadioButton vrai;
-    private CheckBox q2c1, q2c2, q2c3, q2c4, q2c5, q2c6;
+    private CheckBox q2c1;
+    private CheckBox q2c2;
+    private CheckBox q2c3;
+    private CheckBox q2c4;
+    private CheckBox q2c5;
+    private CheckBox q2c6;
     private EditText playersName;
+    private EditText finalQ6A;
+    private String c1 = "California";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 vv.start();
             }
         });
-        Uri uri = Uri.parse("android.resource://com.example.android.skatequizproject3/"+ R.raw.kickflip);
+        Uri uri = Uri.parse("android.resource://com.example.android.skatescrollview/"+ R.raw.kickflip);
 
         vv.setVideoURI(uri);
 
@@ -43,24 +53,24 @@ public class MainActivity extends AppCompatActivity {
 
         vv.start();
 
-
-        //Initialize RadioButton for question 1
+        //Initialize RadioButtons for question 1
         rbKickFLIP = findViewById(R.id.rbKickFlip);
         //Initialize Checkboxes for question 2
         q2c1 = findViewById(R.id.checkSwitch);
         q2c2 = findViewById(R.id.checkRegular);
         q2c3 = findViewById(R.id.checkGoofy);
         q2c4 = findViewById(R.id.checkDaffy);
-        q2c5 = findViewById(R.id.checkDisney);
-        q2c6 = findViewById(R.id.checkFakie);
-        //Initialize RadioButton for question 3
-        ques3Radio1 = findViewById(R.id.melongrab);
+        q2c5 = findViewById(R.id.checkFakie);
+        q2c6 = findViewById(R.id.checkDisney);
+        //Initialize RadioButtons for question 3
+        q3RB2 = findViewById(R.id.melongrab);
         //Initialize RadioButton for question 4
         frontFootPush = findViewById(R.id.q4rB4);
         //Initialize RadioButton for question 5
         vrai = findViewById(R.id.vrai);
-        //Initialize EditText
+        //Initialize EditTexts
         playersName = findViewById(R.id.enterName);
+        finalQ6A = findViewById(R.id.finalQ6);
     }
     // This method is called when the Submit Button is clicked.
     public void sendMessage(View view) {
@@ -68,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
         boolean rbKickFlip = rbKickFLIP.isChecked();
 
-        boolean rbMelonGrab = ques3Radio1.isChecked();
+        boolean rbMelonGrab = q3RB2.isChecked();
 
         boolean checkedSwitch = q2c1.isChecked();
 
@@ -76,22 +86,23 @@ public class MainActivity extends AppCompatActivity {
 
         boolean checkedGoofy = q2c3.isChecked();
 
-        boolean checkedDaffy = !q2c4.isChecked();
+        boolean checkedDaffy = q2c4.isChecked();
 
-        boolean checkedDisney = !q2c5.isChecked();
+        boolean checkedFakie = q2c5.isChecked();
 
-        boolean checkedFakie = !q2c6.isChecked();
+        boolean checkedDisney = q2c6.isChecked();
 
         boolean frontFootPushing = frontFootPush.isChecked();
 
         boolean cestvrai = vrai.isChecked();
 
+        boolean finalQans = finalQ6A.getText().toString().equals(c1) == true;
 
-        int score = calculateScore(rbKickFlip, rbMelonGrab, checkedFakie, checkedDisney, checkedDaffy, checkedGoofy, checkedRegular, checkedSwitch, frontFootPushing, cestvrai);
+        int score = calculateScore(rbKickFlip, rbMelonGrab, checkedGoofy, checkedRegular, checkedSwitch, checkedDaffy, checkedFakie, checkedDisney, frontFootPushing, cestvrai, finalQans);
         String playerMessage = gameSummary(score, playerName);
     }
     //This method calculates the score
-    private int calculateScore(boolean rbKickFlip, boolean checkedFakie, boolean checkedDisney, boolean checkedDaffy, boolean rbMelonGrab, boolean checkedGoofy, boolean checkedRegular, boolean checkedSwitch, boolean frontFootPushing, boolean cestvrai) {
+    private int calculateScore(boolean rbKickFlip, boolean rbMelonGrab, boolean checkedGoofy, boolean checkedRegular, boolean checkedSwitch, boolean checkedDaffy, boolean checkedFakie, boolean checkedDisney, boolean frontFootPushing, boolean cestvrai, boolean finalQans) {
         int score = 0;
         if (rbKickFlip) {
             score += 1;
@@ -99,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
         if (rbMelonGrab) {
             score += 1;
         }
-        if (checkedRegular && checkedGoofy && checkedSwitch && checkedDisney && checkedDaffy && checkedFakie) {
+        if(checkedGoofy && checkedRegular && !checkedDaffy && !checkedDisney && !checkedFakie && checkedSwitch) {
             score += 1;
         }
         if (frontFootPushing) {
@@ -108,16 +119,19 @@ public class MainActivity extends AppCompatActivity {
         if (cestvrai) {
             score += 1;
         }
+        if (finalQans); {
+            score += 1;
+        }
         return score;
     }
     //Game Summary called when player clicks submit
     private String gameSummary(int score, String playerName) {
-        String endOFgame = getString(R.string.wellDone) + " " + playerName + getString(R.string.exclamationMark) + getString(R.string.finalScore) + score + getString(R.string.out7);
+        String endOFgame = getString(R.string.wellDone) + playerName + getString(R.string.exclamationMark) + getString(R.string.finalScore) + score + getString(R.string.out7);
         if (score < 1) {
             Toast.makeText(this, getString(R.string.ansSomething) + " " + playerName + getString(R.string.period), Toast.LENGTH_LONG).show();
             return getString(R.string.nadaComment);
         } else if (score >= 1 && score <= 3) {
-            Toast.makeText(this, getString(R.string.almostLanded) + " " + score + getString(R.string.resetTRY), Toast.LENGTH_LONG).show();
+            Toast.makeText(this, getString(R.string.almostLanded) + score + getString(R.string.resetTRY), Toast.LENGTH_LONG).show();
             return endOFgame;
         } else {
             Toast.makeText(this, getString(R.string.wellDone) + " " + playerName + getString(R.string.merci) + score + " " + getString(R.string.out7) + getString(R.string.exclamationMark), Toast.LENGTH_LONG).show();
